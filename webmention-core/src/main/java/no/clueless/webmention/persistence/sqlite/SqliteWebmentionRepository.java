7f54992ecf9bb2a5b96928566028249e6aaa509a
maritims <maritim@gmail.com>
2026-02-08 19:55:33 +0100

    Sqlite path verification, shader plugin and Dockerfile improvements
    Added path validation to SqliteWebmentionRepository to provide users with a better understanding if something goes wrong during database initialization.
    
    Introduced maven-shader-plugin in webmention-service.
    
    Introduced more flexibility in the webmention-service Dockerfile to support building either for development or for production.

--- webmention-core/src/main/java/no/clueless/webmention/persistence/sqlite/SqliteWebmentionRepository.java
+++ webmention-core/src/main/java/no/clueless/webmention/persistence/sqlite/SqliteWebmentionRepository.java
@@ -5,6 +5,11 @@ import no.clueless.webmention.persistence.WebmentionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,7 +21,7 @@ import java.util.List;
 
 public class SqliteWebmentionRepository implements WebmentionRepository<Integer> {
     private static final Logger log = LoggerFactory.getLogger(SqliteWebmentionRepository.class);
-    private final String connectionString;
+    private final        String connectionString;
 
     public SqliteWebmentionRepository(String connectionString) {
         if (connectionString == null || connectionString.isBlank()) {
@@ -25,8 +30,60 @@ public class SqliteWebmentionRepository implements WebmentionRepository<Integer>
         this.connectionString = connectionString;
     }
 
+    /**
+     * Extracts the last part of the connection string, which is the path to the database file.
+     *
+     * @param connectionString The connection string.
+     * @return The path to the database file.
+     */
+    static Path extractAbsoluteDatabasePath(String connectionString) {
+        if (connectionString == null || connectionString.isBlank()) {
+            throw new IllegalArgumentException("connectionString cannot be null or empty");
+        }
+        if (!connectionString.startsWith("jdbc:sqlite:")) {
+            throw new IllegalArgumentException("connectionString must start with jdbc:sqlite:, but was " + connectionString);
+        }
+
+        var rawPath     = connectionString.replaceFirst("^(jdbc:)?sqlite:", "");
+        var decodedPath = URLDecoder.decode(rawPath, StandardCharsets.UTF_8);
+        return Paths.get(decodedPath).normalize();
+    }
+
+    /**
+     * Verify that the database directory path exists and is writable.
+     *
+     * @param databaseFilePath The path to the database file. Cannot be null nor a directory.
+     * @return True if the database directory path exists and is writable.
+     * @throws IllegalArgumentException If the database file path is null, does not end with .db or is a directory.
+     */
+    static boolean verifyDatabasePath(Path databaseFilePath) {
+        if (databaseFilePath == null) {
+            throw new IllegalArgumentException("databasePath cannot be null");
+        }
+        if (!databaseFilePath.toString().endsWith(".db")) {
+            throw new IllegalArgumentException("databasePath must end with .db, but was " + databaseFilePath);
+        }
+        if (Files.isDirectory(databaseFilePath)) {
+            throw new IllegalArgumentException("databasePath must be a file, but was a directory: " + databaseFilePath);
+        }
+
+        var databaseDirPath = databaseFilePath.toAbsolutePath().getParent();
+        if (!Files.exists(databaseDirPath)) {
+            throw new IllegalArgumentException("Database directory does not exist: " + databaseDirPath);
+        }
+        if (!databaseDirPath.toFile().canWrite()) {
+            throw new IllegalArgumentException("Cannot write to database directory " + databaseDirPath);
+        }
+
+        return true;
+    }
+
     @Override
     public WebmentionRepository<Integer> initialize() {
+        if (!verifyDatabasePath(extractAbsoluteDatabasePath(connectionString))) {
+            throw new RuntimeException("Database path did not pass verification. The connection string must be of the form jdbc:sqlite:path/to/database.db, but was " + connectionString);
+        }
+
         try (var connection = DriverManager.getConnection(connectionString);
              var statement = connection.createStatement()) {
             statement.execute("""
